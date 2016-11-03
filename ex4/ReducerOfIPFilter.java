596e46eca595f498ad5b1e7258812f5613c3cd9f
bert <779152076@qq.com>
2016-11-03 18:41:12 +0800

    ex4
    ex4

--- ex4/ReducerOfIPFilter.java
+++ ex4/ReducerOfIPFilter.java
@@ -1,12 +1,19 @@
 package No4;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class ReducerOfIPFilter {
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		while (scanner.hasNext()){
-			String line = scanner.nextLine();
+		    String line = scanner.nextLine();
+			Set<String> set = new HashSet();
+			while (scanner.hasNext()){
+			if(scanner.nextLine()!=null){
+				set.add(scanner.nextLine());
+			}
 		}
+			System.out.println(set.size());
 	}
 }
